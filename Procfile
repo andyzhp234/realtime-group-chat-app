4163c1c20d29f6049b105ed814e95ed7b83fc747
Haopeng Zeng <andyzhp234@gmail.com>
2022-10-19 09:50:20 -0700

    modified setting

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
-web: gunicorn main.wsgi --log-file -
+web: daphne main.asgi:application --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channels --settings=main.settings -v2
--- main/settings.py
+++ main/settings.py
@@ -204,5 +204,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://*"
+    "http://realtime-group-chat-andyzhp.herokuapp.com/lobby/game",
+    # "http://*"
 ]
