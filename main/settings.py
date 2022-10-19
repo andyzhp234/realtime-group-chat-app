423a4cf25a8f61146a959572f73860b775c23216
Haopeng Zeng <andyzhp234@gmail.com>
2022-10-19 09:54:30 -0700

    modified static

--- main/settings.py
+++ main/settings.py
@@ -204,6 +204,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://realtime-group-chat-andyzhp.herokuapp.com",
     # "http://*"
 ]
