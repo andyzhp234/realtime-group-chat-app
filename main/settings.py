bd22877754c262f247eda94f07db4ef25695d49d
Haopeng Zeng <andyzhp234@gmail.com>
2022-10-19 17:13:53 -0700

    changed to production mode

--- main/settings.py
+++ main/settings.py
@@ -172,10 +172,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static/static'))
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/static'),
-]
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static/static'))
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
